3d9f3cf575f4f719223873766ecb5e81a408367c
thebeardedguru <charles@thebeardedguru.com>
2020-12-24 13:06:01 -0500

    edited firebase.utils with correct firestore config

--- client/src/firebase/firebase.utils.js
+++ client/src/firebase/firebase.utils.js
@@ -3,13 +3,14 @@ import 'firebase/firestore';
 import 'firebase/auth';
 
 const config = {
-  apiKey: 'AIzaSyCdHT-AYHXjF7wOrfAchX4PIm3cSj5tn14',
-  authDomain: 'crwn-db.firebaseapp.com',
-  databaseURL: 'https://crwn-db.firebaseio.com',
-  projectId: 'crwn-db',
-  storageBucket: 'crwn-db.appspot.com',
-  messagingSenderId: '850995411664',
-  appId: '1:850995411664:web:7ddc01d597846f65'
+  apiKey: "AIzaSyBpDrXLD2qOtBrcjE8XTsYDVQ9XJnN8_kg",
+  authDomain: "liquid-clothing-db.firebaseapp.com",
+  databaseURL: "https://liquid-clothing-db-default-rtdb.firebaseio.com",
+  projectId: "liquid-clothing-db",
+  storageBucket: "liquid-clothing-db.appspot.com",
+  messagingSenderId: "684472194535",
+  appId: "1:684472194535:web:244338030b7f3f162f9942",
+  measurementId: "G-DR1FH2JESG"
 };
 
 firebase.initializeApp(config);
